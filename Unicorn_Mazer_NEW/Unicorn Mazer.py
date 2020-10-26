21ed509871ac44fabc20cc5bd5ee2edc64f9d26f
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-10-26 17:10:27 -0300

    proporsion bug fixed

--- Unicorn_Mazer_NEW/Unicorn Mazer.py
+++ Unicorn_Mazer_NEW/Unicorn Mazer.py
@@ -29,7 +29,7 @@ def num6dig(num): # always keep six numbers in the score
     return ''.join(l)
 
 pygame.init() # start pygame system
-start_size = (800, 600) # start size of the screen when the game opens
+start_size = (800, 500) # start size of the screen when the game opens
 screen = pygame.display.set_mode((start_size),RESIZABLE) # create the screen
 second_screen = screen.copy() # create the second screen
 
@@ -59,13 +59,12 @@ while True: # game main loop
 
         elif event.type == VIDEORESIZE: # when the player resize the window
             
-            # keep the window proporsions
-            new_size = event.dict['size']
+            new_size = event.dict['size'] # get the new size
             new_size = list(new_size)
-            new_size[1] = int(new_size[0]-(new_size[0]*0.2)) 
+            new_size[1] = int(new_size[0]*0.625) # keep the window proporsions
 
             # apply the new size into the window
             screen = pygame.display.set_mode(new_size,RESIZABLE)
             second_screen.blit(uni_right, (10, 10))
-            screen.blit(pygame.transform.scale(second_screen, event.dict['size']), (0, 0))
+            screen.blit(pygame.transform.scale(second_screen, new_size), (0, 0))
             pygame.display.flip()
