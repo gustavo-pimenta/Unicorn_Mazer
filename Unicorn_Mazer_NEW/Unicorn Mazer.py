52244d3203b9149dd15c13c4bf2a756a07d6547d
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-10-27 16:35:00 -0300

    fixing screen resize bugs

--- Unicorn_Mazer_NEW/Unicorn Mazer.py
+++ Unicorn_Mazer_NEW/Unicorn Mazer.py
@@ -28,44 +28,38 @@ def num6dig(num): # always keep six numbers in the score
         l.pop(0)
     return ''.join(l)
 
+def screen_print(sprite, size, pos):
+    second_screen.blit(pygame.transform.scale(sprite, size), pos)
+
 pygame.init() # start pygame system
 width = 500 # initial width of the screen when the game opens
 height = 800 # initial height of the screen when the game opens
 screen = pygame.display.set_mode((height,width),RESIZABLE) # create the screen
 second_screen = screen.copy() # create the second screen
 
-
-
-
-uni = pygame.image.load('game_files/images/uni.png')
-    
-
-
 start_menu = True
+uni = pygame.image.load('game_files/images/uni.png')
 
 
 while True: # game main loop
-
     
     while start_menu:
 
-        pygame.event.pump()
-        event = pygame.event.wait()
+        for event in pygame.event.get():
         
-        if event.type == QUIT: # when the player close the game
-            pygame.display.quit()
+            if event.type == QUIT: # when the player close the game
+                pygame.display.quit()
 
-        elif event.type == VIDEORESIZE: # when the player resize the window
-            
-            new_size = event.dict['size'] # get the new size
-            new_size = list(new_size)
-            new_size[1] = int(new_size[0]*0.625) # keep the window proporsions
-            width, height = new_size 
-            print('SCREEN RESOLUTION = (', width, ',', height, ')')
-            
-            # apply the new size into the window
-            screen = pygame.display.set_mode(new_size,RESIZABLE)
-            second_screen.blit(uni, (10, 10))
-            screen.blit(pygame.transform.scale(second_screen, new_size), (0, 0))
+            elif event.type == VIDEORESIZE: # when the player resize the window
+                
+                new_size = event.dict['size'] # get the new size
+                new_size = list(new_size) # turns the tuple into a list
+                new_size[1] = int(new_size[0]*0.625) # keep the window proporsions
+                height, width = new_size # update vars with the new size value
+                screen = pygame.display.set_mode((height,width),RESIZABLE) # recreate the screen with the new size
+                print('SCREEN RESOLUTION = (', width, ',', height, ')') # print output 
 
-            pygame.display.flip()
+        screen_print(uni, (100,100), (300,300))
+
+        screen.blit(pygame.transform.scale(second_screen,(height,width)), (0, 0)) 
+        pygame.display.flip()
