688060650f5ce3c8cc934ebb8a59189ba4c89740
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-11-06 17:21:59 -0300

    finish the event reader

--- Game Code/debug folder/Unicorn Mazer Code.py
+++ Game Code/debug folder/Unicorn Mazer Code.py
@@ -63,6 +63,39 @@ def break_move(): # break all the movement off the unicorn to change stage
     moving_left = False
     moving_right = False
 
+def event_reader(): # read and direct all screen events 
+    
+    global moving_up, moving_down, moving_left, moving_right, height, width
+
+    for event in pygame.event.get():
+        
+        if event.type == QUIT: pygame.display.quit() # when the player close the game
+
+        elif event.type == VIDEORESIZE: # when the player resize the window
+            
+            new_size = event.dict['size'] # get the new size
+            new_size = list(new_size) # turns the tuple into a list
+            new_size[1] = int(new_size[0]*0.625) # keep the window proporsions
+            height, width = new_size # update vars with the new size value
+            screen = pygame.display.set_mode((height,width),RESIZABLE) # recreate the screen with the new size
+            print('SCREEN RESOLUTION = (', width, ',', height, ')') # print output 
+
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP: moving_up = True
+        elif event.type == pygame.KEYUP and event.key == pygame.K_UP: moving_up = False
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN: moving_down = True
+        elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN: moving_down = False
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT: moving_right = True
+        elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT: moving_right = False
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT: moving_left = True
+        elif event.type == pygame.KEYUP and event.key == pygame.K_LEFT: moving_left = False
+
+    try: uni_group.update(moving_up, moving_down, moving_left, moving_right) # send info to update move
+    except: print('ALERT: MOVING ERROR')
+
 class Unicorn(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -113,34 +146,7 @@ while True: # game main loop
 
     while start_menu:
 
-        for event in pygame.event.get():
-        
-            if event.type == QUIT: pygame.display.quit() # when the player close the game
-
-            elif event.type == VIDEORESIZE: # when the player resize the window
-                
-                new_size = event.dict['size'] # get the new size
-                new_size = list(new_size) # turns the tuple into a list
-                new_size[1] = int(new_size[0]*0.625) # keep the window proporsions
-                height, width = new_size # update vars with the new size value
-                screen = pygame.display.set_mode((height,width),RESIZABLE) # recreate the screen with the new size
-                print('SCREEN RESOLUTION = (', width, ',', height, ')') # print output 
-
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP: moving_up = True
-            elif event.type == pygame.KEYUP and event.key == pygame.K_UP: moving_up = False
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN: moving_down = True
-            elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN: moving_down = False
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT: moving_right = True
-            elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT: moving_right = False
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT: moving_left = True
-            elif event.type == pygame.KEYUP and event.key == pygame.K_LEFT: moving_left = False
-
-        try: uni_group.update(moving_up, moving_down, moving_left, moving_right) # send info to update move
-        except: print('ALERT: MOVING ERROR')
+        event_reader()
 
         uni_group.draw(second_screen)
         screen.blit(pygame.transform.scale(second_screen,(height,width)), (0, 0)) # draw the second screen itens into the main screen
